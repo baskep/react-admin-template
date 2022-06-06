ff2ba2a7ba0c5e10be0fdd5f159b601e7c8b6a73
baskep <zt@zhangtaodeMacBook-Pro.local>
2022-06-06 15:55:09 +0800

    feat: 删除footer

--- src/pages/layout/footer.jsx
+++ src/pages/layout/footer.jsx
@@ -1,10 +1,5 @@
-import React from 'react'
-import { Layout } from 'antd'
-
-const { Footer } = Layout
-
 const CustomFooter = () => {
-  return <Footer className="footer">react-admin-template &copy;2022 Created By 早起的老年人</Footer>
+  return null
 }
 
 export default CustomFooter
